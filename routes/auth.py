cd59180ce8fe3df866d73e40427cf25e08ba9951
Manoj-Mahat0 <codeinlastbench@gmail.com>
2025-04-21 12:36:48 +0530

    local2

--- routes/auth.py
+++ routes/auth.py
@@ -14,10 +14,8 @@ def login(user: UserLogin):
     
     token = create_access_token({
         "email": db_user["email"],
-        "role": db_user["role"]
     })
     
     return {
         "access_token": token,
-        "role": db_user["role"]
     }
